d78f4491ffa0f1680a2128dfb7290aa044293b84
hyemch <blossoming33@gmail.com>
2024-10-22 14:15:49 +0900

    feat: 활동내역 카드 컴포넌트 추가

---
+++ one-hada/components/molecules/HistoryCard.tsx
@@ -0,0 +1,30 @@
+import { ChevronRight } from 'lucide-react';
+import { Button } from '../ui/button';
+
+type HistoryCardProps = { name: string; date: string; isConsulting: boolean };
+
+export default function HistoryCard({
+  name,
+  date,
+  isConsulting = false,
+}: HistoryCardProps) {
+  console.log('🚀 ~ isConsulting:', isConsulting);
+  console.log('🚀 ~ date:', date);
+  console.log('🚀 ~ name:', name);
+
+  return (
+    <>
+      <div className='bg-white shadow-md rounded-lg border-l-[10px] border-[#AEDBCE] m-4 mx-6 p-4 px-5 h-20 flex justify-between'>
+        <div className='flex flex-col gap-1'>
+          <h1 className='font-medium text-lg'>{name}</h1>
+          <label className='font-light text-gray-500 text-sm'>{date}</label>
+        </div>
+        <div>
+          <Button className='rounded-full bg-[#61B89F] hover:bg-[#377b68]'>
+            등록 <ChevronRight />
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
