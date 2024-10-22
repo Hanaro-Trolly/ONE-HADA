44437c157903cf8bf4fa0c514a7498d3f21b81f4
hyemch <blossoming33@gmail.com>
2024-10-22 16:21:39 +0900

    feat: 바로가기 카드 컴포넌트 추가 && 테스트 추가

---
+++ one-hada/components/molecules/ShortCutCard.tsx
@@ -0,0 +1,48 @@
+import { StarFilledIcon } from '@radix-ui/react-icons';
+import { Button } from '../ui/button';
+
+type ShortCutCardProps = {
+  id: string;
+  name: string;
+  isEdit: boolean;
+  isFavorite: boolean;
+};
+
+export default function ShortCutCard({
+  id,
+  name,
+  isEdit = false,
+  isFavorite = false,
+}: ShortCutCardProps) {
+  return (
+    <>
+      <div
+        key={id}
+        className='bg-white shadow-md rounded-lg border-l-[10px] border-[#AEDBCE] m-4 mx-6 p-4 px-5 h-15 flex justify-between'
+      >
+        <div className='flex items-center gap-1'>
+          <label className='font-medium text-lg'>{name}</label>
+        </div>
+
+        {isEdit ? (
+          <div className='px-6 py-2'>
+            <input type='checkbox'></input>
+          </div>
+        ) : (
+          <div>
+            <Button
+              variant='ghost'
+              className='bg-opacity-0 hover:bg-white [&_svg]:size-6'
+            >
+              {isFavorite ? (
+                <StarFilledIcon className='text-yellow-300' />
+              ) : (
+                <StarFilledIcon className='text-gray-400' />
+              )}
+            </Button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
