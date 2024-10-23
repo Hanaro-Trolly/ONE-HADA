153840ea5356573ea4f5cbadc86dcfe5024e8ec1
HEE <rkdgml3577@gmail.com>
2024-10-23 14:23:02 +0900

    feat : 조회하기(계좌선택)페이지 기능 완료

--- one-hada/app/check/[accountid]/page.tsx
+++ one-hada/app/check/[accountid]/page.tsx
@@ -1,8 +1,9 @@
+import AccountTypeButton from '@/components/molecules/AccountTypeButton';
+
 export default function AccountTest() {
   return (
     <>
       <div className='font-bold'>폰트 테스트</div>
-      <div></div>
     </>
   );
 }
