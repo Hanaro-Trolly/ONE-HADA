890b7d9ed8979a596cd2a00be21d773eefeea977
Sionparadox <sion9999@naver.com>
2024-10-24 13:34:12 +0900

    fix: 상담내역카드 배경색 수정

--- one-hada/components/molecules/ConsultationCard.tsx
+++ one-hada/components/molecules/ConsultationCard.tsx
@@ -25,7 +25,7 @@ export default function ConsultationCard({
   };
   return (
     <>
-      <div className='w-full h-20 flex justify-between border-b p-2 px-5'>
+      <div className='w-full h-20 flex justify-between border-b p-2 px-5 bg-white'>
         <div className='ml-2 flex flex-col gap-1 justify-center'>
           <div className='text-[#839AA8] text-xs'>{date}</div>
           <div
