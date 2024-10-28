b5edb899b9a0ac9b6af50e56fa424840af987479
Sionparadox <sion9999@naver.com>
2024-10-28 09:17:15 +0900

    fix: 활동내역 페이지 린트에러

--- one-hada/app/(site)/activity/page.tsx
+++ one-hada/app/(site)/activity/page.tsx
@@ -7,6 +7,7 @@ export default function ActivityPage() {
   const router = useRouter();
   useEffect(() => {
     router.push('/activity/history');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div></div>;
 }
