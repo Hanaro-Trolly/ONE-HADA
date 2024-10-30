278a20d92fc9531223fd5ac6e6da49bc68b872f2
mingwan <ibini12@gmail.com>
2024-10-30 09:32:37 +0900

    fix: header padding

--- one-hada/components/admin/AdminHeader.tsx
+++ one-hada/components/admin/AdminHeader.tsx
@@ -103,7 +103,7 @@ export default function AdminHeader() {
 
   return (
     <div className='min-h-screen bg-gray-50'>
-      <div className='max-w-4xl mx-auto p-6'>
+      <div className='max-w-4xl mx-auto p-4'>
         <div className='flex justify-between items-center mb-8'>
           <Title text='목록' />
           <button
