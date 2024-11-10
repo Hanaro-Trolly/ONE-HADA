9fc6fa6a4a6044f16b99022ad69d892178629160
somea82 <joemoon0526@gmail.com>
2024-11-10 17:33:36 +0900

    feat: 소켓 구현(1.0.1)  with BE 1.0.1

---
+++ one-hada/app/(site)/test/page.tsx
@@ -0,0 +1,14 @@
+import Chatting2 from './Chatting2';
+
+export default function Test() {
+  return (
+    <div className='flex'>
+      <div>
+        <Chatting2 />
+      </div>
+      <div>
+        <Chatting2 />
+      </div>
+    </div>
+  );
+}
