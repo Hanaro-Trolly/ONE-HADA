495708fb9311a5993219bedd8e17f927c15d9f16
Sionparadox <sion9999@naver.com>
2024-12-21 15:26:54 +0900

    fix: 추천 캐러셀 로딩 스피너 추가

--- one-hada/components/home/AutoRecommendCarousel.tsx
+++ one-hada/components/home/AutoRecommendCarousel.tsx
@@ -7,19 +7,20 @@ import {
 } from '@/components/ui/carousel';
 import { PRODUCT_LIST } from '@/data/productData';
 import Autoplay from 'embla-carousel-autoplay';
+import { RingLoader } from 'react-spinners';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { Button } from '../ui/button';
 
-interface AutoMessageCarouselProps {
+interface AutoRecommendCarouselProps {
   recommendProductList: string[];
 }
 
-export default function AutoMessageCarousel({
+export default function AutoRecommendCarousel({
   recommendProductList,
-}: AutoMessageCarouselProps) {
-  const [menuType, setMenuType] = useState('loan');
+}: AutoRecommendCarouselProps) {
+  const [menuType, setMenuType] = useState('none');
 
   const formatMenuUrl = (productId: string) => {
     const matches = productId.match(/product(\w+)(\d+)/);
@@ -40,50 +41,56 @@ export default function AutoMessageCarousel({
 
   return (
     <div className='flex justify-center'>
-      <Carousel
-        opts={{
-          align: 'center',
-          loop: true,
-        }}
-        plugins={[
-          Autoplay({
-            delay: 5000,
-          }),
-        ]}
-        className='h-16 w-full flex justify-between items-center'
-      >
-        <CarouselContent showDots={false}>
-          {recommendProductList.map((productId, index) => (
-            <CarouselItem key={index}>
-              <Link href={formatMenuUrl(productId)}>
-                <Button
-                  id={'homeButton' + productId}
-                  variant='home'
-                  className='h-16 w-full mx-2 font-medium rounded-x bg-white hover:bg-[#F0F0F0] flex justify-between'
-                >
-                  <div className='flex flex-col'>
-                    <label className='text-black'>
-                      {PRODUCT_LIST[menuType].products[index]}
-                    </label>
-                    <label className='text-xs text-slate-600'>
-                      {PRODUCT_LIST[menuType].oneline[index]}
-                    </label>
-                  </div>
-                  <div>
-                    <Image
-                      src={`/products/${menuType}${index}.png`}
-                      alt={`${menuType} ${index}`}
-                      width={40}
-                      height={40}
-                      priority
-                    />
-                  </div>
-                </Button>
-              </Link>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
+      {menuType !== 'none' ? (
+        <Carousel
+          opts={{
+            align: 'center',
+            loop: true,
+          }}
+          plugins={[
+            Autoplay({
+              delay: 5000,
+            }),
+          ]}
+          className='h-16 w-full flex justify-between items-center'
+        >
+          <CarouselContent showDots={false}>
+            {recommendProductList.map((productId, index) => (
+              <CarouselItem key={index}>
+                <Link href={formatMenuUrl(productId)}>
+                  <Button
+                    id={'homeButton' + productId}
+                    variant='ghost'
+                    className='h-16 w-full mx-2 font-medium rounded-x bg-white hover:bg-[#F0F0F0] flex justify-between'
+                  >
+                    <div className='flex flex-col text-lg'>
+                      <label className='text-black'>
+                        {PRODUCT_LIST[menuType].products[index]}
+                      </label>
+                      <label className='text-xs text-slate-600'>
+                        {PRODUCT_LIST[menuType].oneline[index]}
+                      </label>
+                    </div>
+                    <div>
+                      <Image
+                        src={`/products/${menuType}${index}.png`}
+                        alt={`${menuType} ${index}`}
+                        width={40}
+                        height={40}
+                        priority
+                      />
+                    </div>
+                  </Button>
+                </Link>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      ) : (
+        <div className='flex justify-center items-center h-16 w-full mx-2'>
+          <RingLoader color='#61B89F' size={40} />
+        </div>
+      )}
     </div>
   );
 }
